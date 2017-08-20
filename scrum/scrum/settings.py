7cd1051747e03a65b598e01b06693f6b30e5a22c
Hendrik Frentrup <hendrik.frentrup@panaseer.com>
2017-08-20 21:31:36 +0100

    consolidating the Ch3 sitebuilder app and the Ch4-6 scrum board into a single repo

--- scrum/scrum/settings.py
+++ scrum/scrum/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
-
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
@@ -73,8 +72,10 @@ WSGI_APPLICATION = 'scrum.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'scrum',
+        # 'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        # 'NAME': 'scrum',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, '../scrum.sqlite3'),
     }
 }
 
